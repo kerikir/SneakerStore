a7a381596d1553726cb09a1b24644c25b7b9bf72
kerikir <vlasov-kerik@mail.ru>
2026-04-25 17:38:28 +0300

    added the library RecyclerView in the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.firebase.database)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
