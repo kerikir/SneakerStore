9f4eabd4159c0816abaca33792dc10694e29c096
kerikir <vlasov-kerik@mail.ru>
2026-06-19 15:03:32 +0300

    the selected category is implemented in the adapter

--- app/src/main/java/com/kerikir/sneakerstore/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/kerikir/sneakerstore/adapter/CategoryAdapter.kt
@@ -1,15 +1,23 @@
 package com.kerikir.sneakerstore.adapter
 
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.kerikir.sneakerstore.R
 import com.kerikir.sneakerstore.databinding.ViewholderCategoryBinding
 import com.kerikir.sneakerstore.model.CategoryModel
 
 class CategoryAdapter(
     val items: MutableList<CategoryModel>
 ) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+
+    private var selectedPosition = -1
+    private var lastSelectedPosition = -1
+
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -27,10 +35,30 @@ class CategoryAdapter(
         position: Int
     ) {
         val item = items[position]
+
         holder.binding.titleCategory.text = item.title
         Glide.with(holder.itemView.context)
             .load(item.picUrl)
             .into(holder.binding.picCategory)
+
+        if (selectedPosition == position) {
+            holder.binding.picCategory.setBackgroundResource(R.drawable.green_bg)
+            ImageViewCompat.setImageTintList(
+                holder.binding.picCategory,
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(holder.itemView.context, R.color.white)
+                )
+            )
+
+        } else {
+            holder.binding.picCategory.setBackgroundResource(R.drawable.gray_oval_bg)
+            ImageViewCompat.setImageTintList(
+                holder.binding.picCategory,
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(holder.itemView.context, R.color.black)
+                )
+            )
+        }
     }
 
 
