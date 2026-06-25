8187139b17d98090c652d8dc48aab662bebd4aea
kerikir <vlasov-kerik@mail.ru>
2026-06-25 18:01:40 +0300

    added loading of categories in the main activity

--- app/src/main/java/com/kerikir/sneakerstore/MainActivity.kt
+++ app/src/main/java/com/kerikir/sneakerstore/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.kerikir.sneakerstore
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kerikir.sneakerstore.activity.BaseActivity
+import com.kerikir.sneakerstore.adapter.CategoryAdapter
 import com.kerikir.sneakerstore.databinding.ActivityMainBinding
+import com.kerikir.sneakerstore.viewModel.MainViewModel
 
 class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel = MainViewModel()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initCategories()
+    }
+
+
+    private fun initCategories() {
+        binding.progressBarCategory.visibility = View.VISIBLE
+
+        viewModel.category.observe(this, Observer {
+
+            binding.recyclerViewCategory.layoutManager = LinearLayoutManager(
+                this@MainActivity,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            binding.recyclerViewCategory.adapter = CategoryAdapter(it)
+            binding.progressBarCategory.visibility = View.GONE
+        })
     }
 }
