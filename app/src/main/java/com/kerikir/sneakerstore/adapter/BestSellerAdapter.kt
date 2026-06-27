157a9b722bccdaa7977948edd09402daa406ed74
kerikir <vlasov-kerik@mail.ru>
2026-06-27 11:37:31 +0300

    implemented methods for the adapter for the list of products from bestsellers

--- app/src/main/java/com/kerikir/sneakerstore/adapter/BestSellerAdapter.kt
+++ app/src/main/java/com/kerikir/sneakerstore/adapter/BestSellerAdapter.kt
@@ -1,7 +1,9 @@
 package com.kerikir.sneakerstore.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.kerikir.sneakerstore.databinding.ViewholderBestSellerBinding
 import com.kerikir.sneakerstore.model.ItemsModel
 
@@ -12,16 +14,31 @@ class BestSellerAdapter(val items: MutableList<ItemsModel>) :
         parent: ViewGroup,
         viewType: Int
     ): BestSellerAdapter.ViewHolder {
-        TODO("Not yet implemented")
+
+        val binding = ViewholderBestSellerBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ViewHolder(binding)
     }
+
 
     override fun onBindViewHolder(holder: BestSellerAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.biding.titleShoes.text = items[position].title
+        holder.biding.priceShoes.text = items[position].price.toString() + " USD"
+        holder.biding.ratingBar.rating = items[position].rating.toFloat()
+
+        Glide.with(holder.itemView.context)
+            .load(items[position].picUrl.first())
+            .into(holder.biding.picShoes)
+
+        Glide.with(holder.itemView.context)
+            .load(items[position].logo)
+            .into(holder.biding.picBrand)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+
+    override fun getItemCount(): Int = items.size
+
 
 
     class ViewHolder(val biding: ViewholderBestSellerBinding)
