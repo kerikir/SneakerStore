d20fe3e5d0eae3cb7327e760a7780b9fea029a61
kerikir <vlasov-kerik@mail.ru>
2026-06-29 15:49:03 +0300

    implemented the onCreateViewHolder method in the adapter

--- app/src/main/java/com/kerikir/sneakerstore/adapter/ListColorsAdapter.kt
+++ app/src/main/java/com/kerikir/sneakerstore/adapter/ListColorsAdapter.kt
@@ -1,5 +1,7 @@
 package com.kerikir.sneakerstore.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
@@ -10,12 +12,24 @@ class ListColorsAdapter(
     var picMain: ImageView
 ) : RecyclerView.Adapter<ListColorsAdapter.ViewHolder>() {
 
+    private var selectedPosition = -1
+    private var lastSelectedPosition = -1
+
+    private lateinit var context: Context
+
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        TODO("Not yet implemented")
+
+        context = parent.context
+        val binding = ViewholderColorShoesBinding.inflate(
+            LayoutInflater.from(context), parent, false
+        )
+        return ViewHolder(binding)
     }
+
 
     override fun onBindViewHolder(
         holder: ViewHolder,
@@ -24,9 +38,11 @@ class ListColorsAdapter(
         TODO("Not yet implemented")
     }
 
+
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
     }
+
 
     class ViewHolder(
         val binding: ViewholderColorShoesBinding
