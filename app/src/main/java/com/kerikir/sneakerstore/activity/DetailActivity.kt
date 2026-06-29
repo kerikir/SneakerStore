802c5d2b18af48bf9321537684fa4a9b789c0b00
kerikir <vlasov-kerik@mail.ru>
2026-06-29 16:22:08 +0300

    added fields to Activity

--- app/src/main/java/com/kerikir/sneakerstore/activity/DetailActivity.kt
+++ app/src/main/java/com/kerikir/sneakerstore/activity/DetailActivity.kt
@@ -3,10 +3,24 @@ package com.kerikir.sneakerstore.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kerikir.sneakerstore.R
+import com.kerikir.sneakerstore.databinding.ActivityDetailBinding
+import com.kerikir.sneakerstore.helper.ManagmentCart
+import com.kerikir.sneakerstore.model.ItemsModel
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    private lateinit var item: ItemsModel
+    private var numberOrder = 1
+
+    private lateinit var managementCart: ManagmentCart
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        managementCart = ManagmentCart(this)
     }
 }
