60a7a680c881674f73187373a4679bb24615a5fb
kerikir <vlasov-kerik@mail.ru>
2026-07-02 15:28:09 +0300

    added implementation of adapter methods for cart

--- app/src/main/java/com/kerikir/sneakerstore/adapter/CartAdapter.kt
+++ app/src/main/java/com/kerikir/sneakerstore/adapter/CartAdapter.kt
@@ -1,20 +1,33 @@
 package com.kerikir.sneakerstore.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.kerikir.sneakerstore.databinding.ActivityCartBinding
+import com.kerikir.sneakerstore.databinding.ViewholderCartBinding
+import com.kerikir.sneakerstore.helper.ChangeNumberItemsListener
+import com.kerikir.sneakerstore.helper.ManagmentCart
 import com.kerikir.sneakerstore.model.ItemsModel
 
 class CartAdapter(
-    private val listItemSelected: ArrayList<ItemsModel>
+    private val listItemSelected: ArrayList<ItemsModel>,
+    context: Context,
+    var changeNumberItemsListener: ChangeNumberItemsListener? = null
 ) : RecyclerView.Adapter<CartAdapter.ViewHolder>() {
+
+    private val managementCart = ManagmentCart(context)
+
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        TODO("Not yet implemented")
+        val binding = ViewholderCartBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ViewHolder(binding)
     }
+
 
     override fun onBindViewHolder(
         holder: ViewHolder,
@@ -23,11 +36,12 @@ class CartAdapter(
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+
+    override fun getItemCount(): Int = listItemSelected.size
+
+
 
     class ViewHolder(
-        val binding: ActivityCartBinding
+        val binding: ViewholderCartBinding
     ) : RecyclerView.ViewHolder(binding.root)
 }
